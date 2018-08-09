8aabab8c3550960152db75e7fb8053d761fa568b
Bryan Rodgers <bryan@inomial.com>
2018-08-09 12:30:00 +1000

    Remove commented-out grow invocation example
    Invocation of grow was incorrect - entrypoint script should only ever be
    invoking grow in "upgrade" mode; "init" mode should only ever be invoked by
    systems administrator setting GROW=init environment or invoking grow manually
    outside the container.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,38 +13,7 @@ export DS_PASSWORD=${DS_PASSWORD:-developer}
 export KAFKA=${KAFKA:-kafka:9092}
 export IDP=${IDP:-'http://idp:8080/auth'}
 
-# If your microservice maintains a database schema, you'll need to uncomment the following lines
-# so that Grow can manage your database objects.
-# (n.b. the need to detect for greenfields installs will be redundant once "--grow auto" support
-# is properly implemented in grow).
-
-#echo "Invoking grow (check for/apply database schema updates)"
-#postgres_conninfo_url=${DS_URL#jdbc:}
-#export PGUSER=$DS_USERNAME
-#export PGPASSWORD=$DS_PASSWORD
-#growdir=`mktemp -d`
-#(
-#  # Need to run this in a subshell due to having to change the current directory.
-#  # The inomial.io/openjdk8-jre Docker base image lacks the jar(1) command, but
-#  # has the BusyBox version of unzip(1), so we can use that to unpack the GROW script
-#  # from the application .jar.
-#  unzip -q -d $growdir $APPLICATION_JAR 'GROW/*'
-#  cd $growdir
-#
-#  # Check if we're doing a regular upgrade or greenfields installation (installing on a blank DB)
-#  is_greenfields_install=`psql -d $postgres_conninfo_url -qAtc "select to_regnamespace('grow') is null;"`
-#  if [ "$is_greenfields_install" = t ]; then
-#    # We're installing to a blank database, so grow must be invoked in "init" mode
-#    echo "Greenfields install; grow will be run in 'init' mode for this startup to initialise the DB."
-#    grow_mode=init
-#  else
-#    # Grow is doing a regular update on an existing grow-managed DB
-#    grow_mode=upgrade
-#  fi
-#  # Currently the grow.sh script doesn't have the executable bit set, so we have to manually invoke it with bash.
-#  bash ./GROW/com.inomial/grow/bin/grow.sh --grow $grow_mode
-#)
-#rm -rf $growdir
+# Insert grow invocation here if the microservice uses a database.
 
 echo "Starting $SERVICE with environment:"
 # Need to avoid printing any passwords to the Docker logs, but make it clear that the variables are set.
