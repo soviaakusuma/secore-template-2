ebde3cf0c35e4d793fc3ea617e15af3767f99877
Mark Lillywhite <mark@inomial.com>
2018-10-19 14:26:14 +1100

    Adds prometheus as part of the template Main method, so new services get JVM instrumentation (and other data) by default.

--- src/main/java/com/inomial/template/Main.java
+++ src/main/java/com/inomial/template/Main.java
@@ -1,7 +1,14 @@
 package com.inomial.template;
 
+import com.inomial.secore.mon.MonitoringServer;
+
 public class Main {
   public static void main(String argv[]) {
-    System.out.println(Main.class.getName() + " started");
+  
+    // In general, you should leave this in. By default, secore provides instrumentation for the JVM.
+    // You can also add your own metrics, see the secore README for links.
+    MonitoringServer.Start();
+
+    System.out.println(Main.class.getName() + " started. JVM stats are published on port 7070.");
   }
 }
