b04bb3847ac404c629dcfdc9bbd0ef41f60ee629
Mark Lillywhite <mark@inomial.com>
2019-07-25 13:21:52 +1000

    Adds System.exit() to template Main method.

--- src/main/java/com/inomial/secore/template/Main.java
+++ src/main/java/com/inomial/secore/template/Main.java
@@ -10,5 +10,10 @@ public class Main {
     MonitoringServer.Start();
 
     System.out.println(Main.class.getName() + " started. JVM stats are published on port 7070.");
+    
+    // Explicitly calling System.exit() is REALLY IMPORTANT to prevent processes
+    // entering a live-lock due to daemon threads keeping a dead application running.
+    // Don't remove this line unless you have a really good reason.
+    System.exit(1);
   }
 }
