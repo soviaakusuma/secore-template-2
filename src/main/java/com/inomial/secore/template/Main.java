ea379466a8b4493a803e3b8d5c587d3988344954
david <david@inomial.com>
2020-12-02 16:18:57 +0800

    Use secore defaults for healthchecks

--- src/main/java/com/inomial/secore/template/Main.java
+++ src/main/java/com/inomial/secore/template/Main.java
@@ -4,6 +4,9 @@ package com.inomial.secore.template;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import com.inomial.secore.health.HealthcheckServer;
+import com.inomial.secore.kafka.KafkaProperties;
+
 public class Main {
 
     static HealthcheckServer health;
@@ -24,12 +27,14 @@ public class Main {
             // → → → → → If your application does not use consul, ← ← ← ← ←
             // → → → → → uncomment these and adjust to suit:      ← ← ← ← ←
             //
-            // Map<String, Healthcheck> checks = new HashMap<>();
-            // checks.put("kafka", new KafkaHealthcheck("kafka:9092", 10000L));
-            // String url = String.format("jdbc:postgresql://%s:%s/%s", host, port, database);
-            // checks.put("postgresql", new PostgresHealthcheck(url, user, password));
-            // // … add other checks if you have some …
-            // health.startServer(checks, 150L, MonitoringServer.DEFAULT_PORT);
+//             Map<String, Healthcheck> checks = new HashMap<>();
+//             checks.put("kafka", new KafkaHealthcheck(KafkaProperties.getBootstrapServer(), 10000L));
+//             String url = System.getenv("DS_URL");
+//             String username = System.getenv("DS_USERNAME");
+//             String password = System.getenv("DS_PASSWORD");
+//             checks.put("postgresql", new PostgresHealthcheck(url, username, password));
+//             // … add other checks if you have some …
+//             health.startServer(checks, 150L, MonitoringServer.DEFAULT_PORT);
 
             // → → → → → If your application does not use consul, ← ← ← ← ←
             // → → → → → delete the following line and start your ← ← ← ← ←
