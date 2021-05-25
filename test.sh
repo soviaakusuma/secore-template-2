6f6f6473df75a3b01b9e43b828dfad0bb8d084c6
Ross Dougherty <ross@inomial.com>
2021-05-25 15:26:36 +1000

    update test.sh sql test script (#27)

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -ex
+set -euxo pipefail
 
 cd "$(dirname "$0")"
 
@@ -8,14 +8,15 @@ if [ -e build/version.properties ]; then
   . build/version.properties
   app=${app:-$project}
 else
-  app=${app:-$(basename "`pwd`")}
+  app=${app:-$(basename "$(pwd)")}
 fi
 
+docker_compose_opts="--ansi never"
 export COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME:-${app}-test}
 export COMPOSE_FILE=docker-compose.yml:docker-compose-test.yml
 
 # cleanup anything that could be left behind from a previous run
-docker-compose --no-ansi down --volumes --remove-orphans
+docker-compose $docker_compose_opts down --volumes --remove-orphans
 
 return_status() {
   status=$?
@@ -24,15 +25,15 @@ return_status() {
   # if running on Jenkins then archive the logs + db and destroy the environment
   if [ "${BUILD_URL:-}" ]; then
     mkdir -p test-results
-    docker-compose logs --no-color --timestamps > test-results/docker-compose.log 2>&1
-    PGVERSION_CLIENT=$PGVERSION docker-compose run --rm -T testsql sh -c 'pg_dump -Fc $TEST_DB' > test-results/test.pgc
-    docker-compose --no-ansi down --volumes --remove-orphans
+    docker-compose $docker_compose_opts logs --timestamps > test-results/docker-compose.log 2>&1
+    PGVERSION_CLIENT=${PGVERSION:-} docker-compose $docker_compose_opts run --rm -T testsql sh -c 'pg_dump -Fc $TEST_DB' > test-results/test.pgc
+    docker-compose $docker_compose_opts down --volumes --remove-orphans
   fi
 
-  if [ $status -ne 0 ] && [ "`uname`" = Darwin ]; then
+  if [ $status -ne 0 ] && [ "$(uname)" = Darwin ]; then
     echo
     echo "Run the following to connect to the test database:"
-    echo "  docker-compose run --rm testsql psql $(. .env && echo $APP_POSTGRES_DATABASE)"
+    echo "  COMPOSE_PROJECT_NAME=$COMPOSE_PROJECT_NAME COMPOSE_FILE=$COMPOSE_FILE docker-compose run --rm testsql psql $(. .env && echo "$APP_POSTGRES_DATABASE")"
   fi
 
   exit $status
@@ -40,9 +41,9 @@ return_status() {
 trap return_status EXIT
 
 # start postgres
-docker-compose --no-ansi up -d postgres
+docker-compose $docker_compose_opts up -d postgres
 
-timeout=`which timeout` || true
+timeout=$(which timeout) || true
 timeout() {
   if [ "$timeout" ]; then
     $timeout "$@"
@@ -53,19 +54,20 @@ timeout() {
 }
 
 # wait for postgres to ready up
-timeout 60 docker-compose exec -Tu postgres postgres sh -c "until psql -h 127.0.0.1 -U \"\$POSTGRES_USER\" \"\$POSTGRES_DB\" -ec '\q'; do echo 'Postgres is unavailable - sleeping'; sleep 1; done" </dev/null
+timeout 60 docker-compose $docker_compose_opts exec -Tu postgres postgres sh -c "until psql -h 127.0.0.1 -U \"\$POSTGRES_USER\" \"\$POSTGRES_DB\" -ec '\q'; do echo 'Postgres is unavailable - sleeping'; sleep 1; done" </dev/null
 
 # start container with GROW=init and wait for it to exit (start attached with timeout)
-if [ "$1" = upgrade ] && [ ! "$INITVERSION" ]; then
+INITVERSION=${INITVERSION:-}
+if [ "${1:-}" = upgrade ] && [ ! "$INITVERSION" ]; then
   INITVERSION=$(git tag -n | grep -E '^[2-9][0-9]\.(0[1-9]|1[0-2])\.[1-9]([0-9]+)?\s+Jenkins build from master commit ' | sort -V | tail -n1 | awk '{print$1}')
 fi
 if [ "$INITVERSION" ]; then
   echo "Running Grow init with $INITVERSION"
 fi
-VERSION=${INITVERSION:-latest} GROW=init timeout 60 docker-compose --no-ansi up --exit-code-from $app $app
+VERSION=${INITVERSION:-latest} GROW=init timeout 60 docker-compose $docker_compose_opts up --exit-code-from "$app" "$app"
 if [ "$INITVERSION" ]; then
   expectedVer=$(awk -F. '{printf "%.2d%.2d%.3d\n", $1, $2, $3}' <<<"$INITVERSION")
-  dbVer=$(docker-compose run -T --rm testsql psql "$(. .env && echo $APP_POSTGRES_DATABASE)" -qAtc 'select version from grow.log')
+  dbVer=$(docker-compose $docker_compose_opts run -T --rm testsql psql "$(. .env && echo "$APP_POSTGRES_DATABASE")" -qAtc 'select version from grow.log')
   if [ "$expectedVer" != "$dbVer" ]; then
     echo "Expected Grow version $expectedVer from $INITVERSION but got $dbVer"
     exit 1
@@ -74,4 +76,4 @@ fi
 
 # run sql tests
 TIMEOUT=300
-COMPOSE_HTTP_TIMEOUT=$(($TIMEOUT*2)) timeout $TIMEOUT docker-compose --no-ansi up --exit-code-from testsql testsql
+COMPOSE_HTTP_TIMEOUT=$((TIMEOUT*2)) timeout $TIMEOUT docker-compose $docker_compose_opts up --exit-code-from testsql testsql
